dae02bc9ae071897f1efbbaaf7b7d8f3b8ebb00c
Elliott Spelman <spelman7@gmail.com>
2014-04-04 23:50:50 +0000

    initial commit

---
+++ users/admin.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+from .forms import UserCreationForm, UserChangeForm
+
+class CustomUserAdmin(UserAdmin):
+	form = UserChangeForm
+	add_form = UserCreationForm
+	
+	list_display = ('email', 'name', 'is_superuser')
+	list_filter = ('is_superuser',)
+	
+	fieldsets = (
+		(None, {'fields': {
+			'email', 'name', 'password', 'is_superuser', 'is_active'
+		}}),
+		('Groups', {'fields': {'groups',}})
+	)
+	
+	add_fieldsets = (
+		(None, {
+			'classes': ('wide',),
+			'fields': {'email', 'name', 'password1', 'password2'}
+	}),
+	)
+	
+	search_fields = ('email', 'name')
+	ordering = ('email',)
+	filter_horizontal = ('groups',)
+
+admin.site.register(User, CustomUserAdmin)
---
+++ users/models.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+	def create_user(self, email, name=None, password=None):
+		if not email:
+			raise ValueError('Users must have an email address')
+		
+		user = self.model(
+			email=self.normalize_email(email),
+			name=name
+		)
+		
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
+	
+	def create_superuser(self, email, password, name=None):
+		user = self.create_user(email, name, password=password)
+		user.is_admin = True
+		user.is_superuser = True
+		user.is_active = True
+		user.save(using=self._db)
+		return user
+		
+
+class User(AbstractBaseUser, PermissionsMixin):
+	email = models.EmailField(max_length=100, unique=True)
+	name = models.CharField(max_length=255, null=True, default='')
+	is_active = models.BooleanField(default=False)
+	objects = UserManager()
+	
+	USERNAME_FIELD = 'email'
+	
+	def get_full_name(self):
+		return self.name if self.name else self.email
+		
+	def get_short_name(self):
+		return self.email
+		
+	def __unicode__(self):
+		return self.email
+		
+	@property
+	def is_staff(self):
+		return self.is_superuser
